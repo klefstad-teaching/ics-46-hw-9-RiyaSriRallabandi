90c6fbaf73c6f093658fc00488105d51f1eb0cd8
RiyaSriRallabandi <rallabar@uci.edu>
2025-03-15 09:01:43 +0000

    Implemented the dijkstra_shortest_path function in dijkstras.cpp.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,1 +1,29 @@
 #include "dijkstras.h"
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int n = G.numVertices;
+    vector<int> distances(n, INF);
+    previous.resize(n, -1);
+    vector<bool> visited(n, false);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    distances[source] = 0;
+    pq.push({0, source});
+    
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+        if(visited[u]) continue;
+        visited[u] = true;
+
+        for (const Edge& e : G[u]) {
+            int v = e.dst;
+            int weight = e.weight;
+            if (!visited[v] && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight
+                previous[v] = u;
+                pq.push({distances[v], v});
+            }
+        }
+    } return distances;
+}
