7a33b42fbca370616cf7558df9389162c84ab5a4
RiyaSriRallabandi <rallabar@uci.edu>
2025-03-15 09:57:36 +0000

    Modified print_path function to adjust format.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -40,8 +40,8 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& v, int total) {
     if (v.empty()) {
-        cout << "\nTotal cost: " << total << endl; return;}
+        cout << "\nTotal cost is " << total << endl; return;}
     for (size_t i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
-    } cout << "\nTotal cost: " << total << endl;
+    } cout << "\nTotal cost is " << total << endl;
 }
